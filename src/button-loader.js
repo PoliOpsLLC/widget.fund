e44e09ca4688ecfbd992a18d0aaaf2dc2ea750ef
Derek Hurley <dzhurley@gmail.com>
2018-11-16 17:21:40 -0800

    handle campaigns and fix local button/iframe version loading

--- src/button-loader.js
+++ src/button-loader.js
@@ -1,9 +1,9 @@
 (function(window, document) {
     const container = document.createElement('iframe');
-    const { key, location, employer, local, org, style = '', text = 'Sign Up' } = document.currentScript.dataset;
+    const { key, location, campaign, employer, local, org, style = '', text = 'Sign Up' } = document.currentScript.dataset;
     container.srcdoc = `
         <script src="${process.env.SCRIPT_DOMAIN}/button.${process.env.RELEASE_NAME || window.version}.js"></script>
-        <script>window.init(${JSON.stringify({ key, location, employer, local, org, text, style })})</script>
+        <script>window.init(${JSON.stringify({ key, location, campaign, employer, local, org, text, style })})</script>
     `;
     container.width = document.currentScript.dataset.width;
     container.height = document.currentScript.dataset.height;
--- src/iframe-loader.js
+++ src/iframe-loader.js
@@ -1,9 +1,9 @@
 (function(window, document) {
     const container = document.createElement('iframe');
-    const { key, location, employer, local, org, height, width } = document.currentScript.dataset;
+    const { key, location, campaign, employer, local, org, height, width } = document.currentScript.dataset;
     container.srcdoc = `
         <script src="${process.env.SCRIPT_DOMAIN}/iframe.${process.env.RELEASE_NAME || window.version}.js"></script>
-        <script>window.init(${JSON.stringify({ key, location, employer, local, org })})</script>
+        <script>window.init(${JSON.stringify({ key, location, campaign, employer, local, org })})</script>
     `;
     container.width = width || 480;
     container.height = height || 640;
